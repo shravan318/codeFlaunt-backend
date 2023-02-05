0ccd938847fcd9dd1c8b23ffb0d8d562c6d18fbf
shravan <shravan.318@gmail.com>
2023-02-05 16:38:17 +0530

    set up db

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
-
+const connectDB = require("./config/db");
 const app = express();
-
+connectDB();
 app.get("/", (req, res) => res.send("API running"));
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`server started at ${PORT}`));
